05aa4fc78979ed03ecc5fd1913f2ee93d8ecfc8a
victor <martinsvictor6781@gmail.com>
2023-04-19 20:06:05 -0300

    1115

---
+++ questoes/bee_1115/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            if (x > 0 && y > 0) System.out.println("primeiro");
+            else if (x < 0 && y > 0) System.out.println("segundo");
+            else if (x <  0 && y < 0) System.out.println("terceiro");
+            else if (x > 0 && y < 0) System.out.println("quarto");
+            else break;
+        }
+
+        scanner.close();
+    }
+}
